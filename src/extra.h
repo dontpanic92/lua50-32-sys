71217a2b241110bf66cff1fef2eb35a9a65e4d78
Li Shengqiu <lishengqiu.hit@gmail.com>
2023-12-17 22:25:32 +0800

    add lregister

--- src/extra.h
+++ src/extra.h
@@ -2,4 +2,4 @@
 
 int lcall(lua_State *L, int narg, int clear);
 void lgetglobal(lua_State *L, const char *s);
-void lregister(lua_State *L, const char* n, lua_CFunction* f);
+void lregister(lua_State *L, const char* n, lua_CFunction f);
